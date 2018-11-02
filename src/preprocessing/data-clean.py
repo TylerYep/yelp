5ec299c099c5a4d124be456dcae998c0553ba6e4
Edward Lee <ed-w-lee@github.com>
2018-11-01 22:58:49 -0700

    Investigation and preprocessing for chinese restaurants

---
+++ src/preprocessing/data-clean.py
@@ -0,0 +1,19 @@
+import json
+import collections
+
+# investigation
+with open('data/yelp_academic_dataset_business.json', 'r') as f:
+    with open('data/yelp_toronto.json', 'w') as out:
+        for l in f:
+            ex = json.loads(l)
+            categories = ex[u'categories']
+            if ex[u'city'] == u'Toronto' and categories and (u'Food' in categories or u'Restaurants' in categories):
+                json.dump(ex, out)
+                out.write('\n')
+
+blah = collections.defaultdict(int)
+with open('data/yelp_toronto.json', 'r') as f:
+    with open('data/yelp_toronto.csv', 'w') as out:
+        for l in f:
+            ex = json.loads(l)
+            out.write('{business_id}, {latitude}, {longitude}, "{categories}", {stars}\n'.format(**ex))
---
+++ src/preprocessing/data-investigation.py
@@ -0,0 +1,21 @@
+import json
+import collections
+
+# investigation
+citystate = collections.defaultdict(int)
+state = collections.defaultdict(int)
+with open('data/yelp_academic_dataset_business.json', 'r') as f:
+    for l in f:
+        ex = json.loads(l)
+        citystate[(ex[u'state'], ex[u'city'])] += 1
+        state[ex[u'state']] += 1
+
+li = [(k, citystate[k]) for k in citystate]
+li.sort(key = lambda k: -k[1])
+for p in li[:10]:
+    print p[0], p[1]
+
+l2 = [(k, state[k]) for k in state]
+l2.sort(key = lambda k: -k[1])
+for p in l2[:10]:
+    print p[0], p[1]
