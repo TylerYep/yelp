bf0919d68e1c630373a2db9b73061aa927ff8c87
Tyler Yep <tyep@stanford.edu>
2018-11-06 19:32:59 -0800

    distance matrix

--- src/experimental/tyler.py
+++ src/experimental/tyler.py
@@ -5,12 +5,22 @@ import requests
 
 
 def main():
+    # with open("../yelp_dataset/yelp_business.json") as f:
+    #     data = json.load(f)
+    # Add all addresses and ids maybe
     loc1 = '3000 Fowler Rd'
     loc2 = '455 Arguello Way'
     get_one_distance(loc1, loc2)
 
 
-def get_one_distance(loc1, loc2):
+def get_all_distances(locations_arr):
+    distance_matrix = [[] for _ in len(locations_arr)]
+    for i in range(len(locations_arr)):
+        for j in range(len(locations_arr)):
+            distance_matrix[i][j] = get_distance(locations_arr[i], locations_arr[j])
+
+
+def get_distance(loc1, loc2):
     headr = dict()
     headr['key'] = 'AIzaSyBTHAUwdhwblKNeddbm4hkcCm_AI1pLNb0'
     headr['origins'] = loc1
@@ -21,8 +31,3 @@ def get_one_distance(loc1, loc2):
 
 if __name__ == '__main__':
     main()
-
-# yelpf = open("../yelp_dataset/yelp_business.json", "r")
-# with open("../yelp_dataset/yelp_business.json") as f:
-#     data = json.load(f)
-#     pprint(data)
